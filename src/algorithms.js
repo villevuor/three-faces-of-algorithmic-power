dfd74ec6f63d259560009f8d193b19110c9268cf
Ville Vuorenmaa <villevuor@gmail.com>
2021-10-14 15:24:55 +0300

    Tweak the algorithm to be a little more random

--- src/algorithms.js
+++ src/algorithms.js
@@ -24,18 +24,18 @@ const mostPopularWithSmallVariance = (tracks) => {
   // Step 1: Order by popularity score
   tracks.sort((a, b) => b.popularity - a.popularity)
 
-  // Step 2: Hide 3-5 items by random from the top 15
-  for (let i = 0; i < randomIntFromInterval(3, 5); i++) {
-    tracks.splice(randomIntFromInterval(1, 15), 1);
+  // Step 2: Hide 4-6 items by random from the top 10
+  for (let i = 0; i < randomIntFromInterval(4, 6); i++) {
+    tracks.splice(randomIntFromInterval(1, 10), 1);
   }
 
-  // Step 2: Move by random 5-10 items from the last 20 tracks to among the tops
+  // Step 2: Move by random 10-15 items from the last 30 tracks to among the tops
 
   const size = tracks.length;
 
-  for (let i = 0; i < randomIntFromInterval(5, 10); i++) {
-    const from = randomIntFromInterval(size - 20, size - 1);
-    const to = randomIntFromInterval(0, 12);
+  for (let i = 0; i < randomIntFromInterval(10, 15); i++) {
+    const from = randomIntFromInterval(size - 30, size - 1);
+    const to = randomIntFromInterval(0, 20);
     moveItem(tracks, from, to);
   }
 
